fa5251db521f43f6500721c80af2975e688fb4c1
hzluohb <hzlhbin@tairanchina.com>
2018-11-13 17:25:44 +0800

    增加开启关闭

--- content_script.js
+++ content_script.js
@@ -1,52 +1,51 @@
 
-const window_onload_event = ()=>{
-  window.onload = function(){
+const window_onload_event = () => {
+  window.onload = function () {
     const codes = document.getElementsByTagName('code');
-    for(let i = 0 ; i < codes.length; i ++){
+    for (let i = 0; i < codes.length; i++) {
       let code = codes[i];
       code.style.fontWeight = 'bold';
     }
   };
 };
 
-const getStorage = key => new Promise(resolve =>{
-  chrome.storage.local.get(key,function(result){
+const getStorage = key => new Promise(resolve => {
+  chrome.storage.local.get(key, function (result) {
     resolve(result[key]);
   });
 });
 
-const init = async() => {
+const init = async () => {
   let loopTimes = 0;
+  const flag = await getStorage('CEFWB_SWITCH');
   const head = document.getElementsByTagName('head')[0];
-  const appendChild = ()=>{
-    const ele = document.createElement('style');  
+  const appendChild = () => {
+    const ele = document.createElement('style');
     ele.innerHTML = value;
-    ele.setAttribute('type','text/css'); 
-    ele.setAttribute('id','CEFWB_STYLE'); 
-    head.appendChild(ele); 
+    ele.setAttribute('type', 'text/css');
+    ele.setAttribute('id', 'CEFWB_STYLE');
+    head.appendChild(ele);
   }
   const value = await getStorage('CEFWB_STYLE');
-  appendChild();
-  
+  flag && appendChild();
   const loop = () => {
-    loopTimes ++ ;
-    setTimeout(()=>{
+    loopTimes++;
+    setTimeout(() => {
       const CEFWB_STYLE = document.getElementById('CEFWB_STYLE');
-      if(!CEFWB_STYLE){    
+      if (!CEFWB_STYLE) {
         appendChild();
         loop();
-      }else{
-        if(loopTimes < 10){
+      } else {
+        if (loopTimes < 10) {
           loop();
-        }else{
-          console.log('-------------样式注入完成---------------');          
+        } else {
+          console.log('-------------样式注入完成---------------');
         }
-      }  
-    },500);
+      }
+    }, 500);
   }
-  loop();
-
+  flag && loop();
 };
 init();
 
-  
+
--- popup.js
+++ popup.js
@@ -1,57 +1,80 @@
 (() => {
-  
-  const getStorage = key => new Promise(resolve =>{
-    chrome.storage.local.get(key,function(result){
+
+  const getStorage = key => new Promise(resolve => {
+    chrome.storage.local.get(key, function (result) {
       resolve(result[key]);
     });
   });
 
+  const CreateDiv = (float) => {
+    const div = document.createElement('div');
+    div.style.float = float;
+    div.style.paddingBottom = "10px";
+    return div;
+  };
 
-
-  window.onload = async function () {   
+  window.onload = async function () {
     let time;
-    const value = await getStorage('CEFWB_STYLE'); 
+    const value = await getStorage('CEFWB_STYLE');
+    const flag = await getStorage('CEFWB_SWITCH');
     const p = document.createElement('p');
     p.innerHTML = '设置全局页面样式';
     p.style.textAlign = 'center';
 
+    // 输入框
     const textarea = document.createElement('textarea');
-    textarea.setAttribute('cols', '30');
-    textarea.setAttribute('rows', '5');
+    textarea.setAttribute('cols', '42');
+    textarea.setAttribute('rows', '8');
     value && (textarea.value = value);
 
-    const div = document.createElement('div');
-    const div1 = document.createElement('div');
-
+    // 按钮容器
+    const div = CreateDiv('left')
+    
     const btn = document.createElement('button');
     btn.innerHTML = '确定';
-
     const span = document.createElement('span');
     span.innerHTML = "修改成功~";
     span.style.display = "none";
 
     document.body.appendChild(p);
     document.body.appendChild(textarea);
-
-    document.body.appendChild(div1);
     document.body.appendChild(div);
+
+    // 开关容器
+    const switchOnOffContent = CreateDiv('right')
+    const label = document.createElement('label');
+    const check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    const text = document.createElement('span');
+    flag && (check.checked = true);
+    check.onchange = function () {
+      text.innerText = this.checked ? '开启' : '关闭'
+      chrome.storage.local.set({
+        'CEFWB_SWITCH': this.checked,
+      }, () => { });
+    };
+    text.innerText = check.checked ? '开启' : '关闭';
+    label.appendChild(text);
+    label.appendChild(check);
+    switchOnOffContent.appendChild(label);
+    document.body.appendChild(switchOnOffContent);
 
     div.appendChild(btn);
     div.appendChild(span);
 
+    // 点击事件
     btn.onclick = function () {
       chrome.storage.local.set({
         'CEFWB_STYLE': textarea.value
       }, function (result) {
         clearTimeout(time);
         span.style.display = " inline-block";
-        time = setTimeout(()=>{
+        time = setTimeout(() => {
           span.style.display = 'none';
-        },3000);
+        }, 3000);
       });
     }
   };
-
 })();
 
 
